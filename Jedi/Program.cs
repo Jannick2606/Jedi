c17584397ce5475f0774f4d93122769d0f47cf32
zbcjaso <zbcjaso@ZBC-S-6BN0W>
2021-09-01 10:16:02 +0200

    Add project files.

---
+++ Jedi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Jedi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> nameAge = new Dictionary<string, int>() { };     //Opgave 1
+            nameAge.Add("Jannick", 21);      //Opgave 2
+            nameAge.Add("Name", 50);
+            nameAge.Add("Han", 28);
+            nameAge.Add("Old", 100);
+            Console.WriteLine(nameAge.First());  
+
+            nameAge.Remove("Han");       //Opgave 3
+
+            foreach (var item in nameAge)   //Opgave 4
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
